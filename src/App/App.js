a3f7cd31eaba058c2e344274d630ef66c84656da
AndresQA <34174822+AndresQA@users.noreply.github.com>
2021-03-07 17:33:40 -0500

    source 2

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Home from '../Home/Home';
 import Register from '../Register/Register';
 import Resume from '../Resume/Resume';
-//import './App.css';
+import { HashRouter as Router, Route, link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home/>
-      <Register/>
-      <Resume></Resume>
+      <Router>
+        <Route path="/" exact component={Home}/>
+        <Route path="/registro" component={Register}/>
+        <Route path="/resumen" component={Resume}/>
+      </Router>
+
 
     </div>
   );
