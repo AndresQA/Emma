d079a02cd484bf615415c45375383349813b2dab
AndresQA <34174822+AndresQA@users.noreply.github.com>
2021-03-07 16:52:05 -0500

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from '../Home/Home';
+import Register from '../Register/Register';
+import Resume from '../Resume/Resume';
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Register/>
+      <Resume></Resume>
+
+    </div>
+  );
+}
+
+export default App;
