468620e5a03783f7b8ca04bda5e716e279b5482e
AndresQA <34174822+AndresQA@users.noreply.github.com>
2021-04-09 00:27:51 -0500

    registro ready

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from '../Home/Home';
 import Register from '../Register/Register';
 import Resume from '../Resume/Resume';
@@ -8,22 +8,94 @@ import Test from '../Test/Test';
 import Index from '../Index/Index';
 import firebase from '../Firebase/Firebase';
 import 'firebase/auth';
-firebase.auth().onAuthStateChanged(user => {
-  //console.log(user);
-})
+
 
 function App() {
-  
+
+
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState('');
+
+  const clearInputs = () =>{
+    setEmail('');
+    setPassword('');
+  }
+
+  const clearError= () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  const handleLogin = () => {
+    clearError();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPassword(err.message);
+            break;
+        }
+      })
+  }
+
+  const handleSignUp = () => {
+    clearError();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPassword(err.message);
+            break;
+        }
+      })
+  };
+
+  const handleLogOut = () => {
+    firebase.auth().signOut();
+  };
+
+  const authListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      }else {
+        setUser("");
+      }
+    })
+  };
+
+  useEffect(() => {
+    authListener();
+  }, [])
+
 
   return (
     <div>
       <Router>
-        <Route path="/" exact component={Home}/>
-        <Route path="/registro" component={Register}/>
-        <Route path="/resumen" component={Resume}/>
-        <Route path="/iniciarSesion" component={Login}/>
-        <Route path="/test" component={Test}/>
-        <Route path="/inicio" component={Index}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/registro" render={() => <Register email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleSignUp={handleSignUp}/>} />
+        <Route path="/resumen" component={Resume} />
+        <Route path="/iniciarSesion" component={Login} />
+        <Route path="/test" component={Test} />
+        <Route path="/inicio" component={Index} />
 
       </Router>
 
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { HashRouter as Router, Route, link, Link } from 'react-router-dom';
 import { Button, TextField } from '@material-ui/core';
-import 'firebase/auth';
-import { useFirebaseApp } from 'reactfire';
 
-const Register = () => {
+const Register = (props) => {
 
-    const [name, setName ] = useState('');
-    const [email, setEmail ] = useState('');
-    const [password, setPassword ] = useState('');
+const {email, setEmail, password, setPassword, handleSignUp} = props;
 
-    const firebase = useFirebaseApp();
-
-    const submit = async ()=>{
-        await firebase.auth().createUserWithEmailAndPassword(email, password);
-    }
 
     return (
         <div className="register">
@@ -27,13 +18,12 @@ const Register = () => {
 
                     <h1 className="register__ltitle">Registrate </h1>
                     <div className="register__buttons">
-
-                        <TextField id="outlined-basic" label="Nombre completo" variant="outlined" onChange={ (ev)=> setName(ev.target.value)} />
-                        <TextField id="outlined-basic" label="Correo electronico" variant="outlined" onChange={ (ev)=> setEmail(ev.target.value)}/>
-                        <TextField id="outlined-password-input" type="password" label="Contraseña" variant="outlined" onChange={ (ev)=> setPassword(ev.target.value)}/>
+                        <TextField id="outlined-basic" label="Nombre completo" variant="outlined" value={email} onChange={e => setEmail(e.target.value)}/>
+                        <TextField id="outlined-basic" label="Correo electronico" variant="outlined" value={email} onChange={e => setEmail(e.target.value)}/>
+                        <TextField id="outlined-password-input" type="password" label="Contraseña" variant="outlined" value={password} onChange={e => setPassword(e.target.value)}/>
 
                         <Link to="/inicio">
-                            <Button variant="contained" color="secondary" onClick={submit} style={{
+                            <Button variant="contained" color="secondary" onClick={handleSignUp} style={{
                                 borderRadius: 100,
                                 backgroundColor: "#8349C1",
                                 padding: "16px",
