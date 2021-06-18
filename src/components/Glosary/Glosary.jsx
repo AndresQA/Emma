b60c30adfe87e5e285a432ef317b749a740b9d07
AndresQA <34174822+AndresQA@users.noreply.github.com>
2021-06-17 23:41:23 -0500

    healthmap description add

--- src/components/Glosary/Glosary.jsx
+++ src/components/Glosary/Glosary.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import AppContext from '../../App/AppContext';
 import './Glosary.scss'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 const Glosary = () => {
     const { useFaq } = AppContext.Consumer();
     const [type, Step] = useFaq();
+    const [iconstatus, setIconstatus] = useState(Array(13));
 
     useEffect(() => {
         Step("Notification");
+        for (let i = 0; i < iconstatus.length; i++) {
+            iconstatus[i] = 0;
+        }
     }, [])
 
     const handleClick = (index) => {
-        const title = document.querySelector(".glosary__itemsDescription__"+index);
+        const title = document.querySelector(".glosary__itemsDescription__" + index);
         const imgChindex = false;
+        console.log(iconstatus)
         if (title.style.display == "block") {
             title.style.display = "none";
+            
         } else
             title.style.display = "block";
+
     }
 
 
@@ -24,36 +33,37 @@ const Glosary = () => {
         <div className="glosary">
             <h1>Diccionario del migrante</h1>
             <div className="glosary__items">
-                <div className="glosary__itemsTitle" onClick={()=>handleClick(1)}>
+                <div className="glosary__itemsTitle" onClick={() => handleClick(0)}>
                     <h3>¿Quién es un migrante?</h3>
-                    <img src="/icons/plus.png" alt="" />
+                    {iconstatus[0] == 0 ? <ExpandMoreIcon style={{ fontSize: 55, color: "#7241A6" }} /> : <ExpandLessIcon style={{ fontSize: 55, color: "#7241A6" }} />}
+
                 </div>
-                <div className="glosary__itemsDescription__1">
+                <div className="glosary__itemsDescription__0">
                     <p>En el ámbito internacional no existe aún una definición concreta y universal de lo que es un “migrante”. Sin embargo, ACNUR lo define como una persona que elige dejar libremente su país de origen. Por su lado, Las Naciones Unidas definen al migrante como “Alguien que ha residido en un país extranjero durante más de un año independientemente de las causas de su traslado, voluntario o involuntario, o de los medios utilizados, legales u otros”.
-                    La Declaración de Nueva York en 2016 estableció el Pacto Mundial sobre Migración, el cual no es jurídicamente vinculante, pero se basa en los valores de la soberanía del Estado, la responsabilidad compartida, la no discriminación y los Derechos Humanos2. Además, comprende de 23 objetivos para gestionar mejor la migración a nivel local, nacional, regional y mundial; es decir, este acuerdo es un apoyo ligado a las personas que deciden partir de su país de origen, y por tanto, puedan construir y mantener medios de vida sostenibles en el lugar al que decidan llegar.
+                        La Declaración de Nueva York en 2016 estableció el Pacto Mundial sobre Migración, el cual no es jurídicamente vinculante, pero se basa en los valores de la soberanía del Estado, la responsabilidad compartida, la no discriminación y los Derechos Humanos2. Además, comprende de 23 objetivos para gestionar mejor la migración a nivel local, nacional, regional y mundial; es decir, este acuerdo es un apoyo ligado a las personas que deciden partir de su país de origen, y por tanto, puedan construir y mantener medios de vida sostenibles en el lugar al que decidan llegar.
                     </p>
                 </div>
             </div>
 
             <div className="glosary__items">
-                <div className="glosary__itemsTitle" onClick={()=>handleClick(2)}>
+                <div className="glosary__itemsTitle" onClick={() => handleClick(1)}>
                     <h3>¿Qué es un extranjero?</h3>
-                    <img src="/icons/plus.png" alt="" />
+                    <ExpandMoreIcon style={{ fontSize: 55, color: "#7241A6" }} />
                 </div>
-                <div className="glosary__itemsDescription__2">
+                <div className="glosary__itemsDescription__1">
                     <p>Una persona extranjera es aquella que no es considerado como miembro de la comunidad política que enfrenta, es decir, cuando el entorno en el que se encuentra pertenece a una cultura y costumbre de un país distinto al que es originario. Simultáneamente, es extranjero quien no es ciudadano y no goza de la nacionalidad del país en el que está localizado. En otras palabras, aquel que no adquiere ni ejerce los derechos y responsabilidades inherentes a la pertenencia de ese Estado.3 Los extranjeros cuentan con derechos y deberes por el hecho de permanecer en un determinado territorio. En Colombia, tal como lo expresa el art. 100 de la Constitución Política, en palabras del Ministerio de Relaciones Exteriores “Los extranjeros gozan en el territorio colombiano de los mismos derechos y garantías de los nacionales, pero se encuentran también sometidos por los mismos deberes civiles y están obligados al respeto de la Constitución Política y de la ley” (Sentencia C-1251, 2001).
                     </p>
                 </div>
             </div>
 
             <div className="glosary__items">
-                <div className="glosary__itemsTitle" onClick={()=>handleClick(3)}>
+                <div className="glosary__itemsTitle" onClick={() => handleClick(2)}>
                     <h3>¿Qué es un migrante interno?</h3>
-                    <img src="/icons/plus.png" alt="" />
+                    <ExpandMoreIcon style={{ fontSize: 55, color: "#7241A6" }} />
                 </div>
-                <div className="glosary__itemsDescription__3">
+                <div className="glosary__itemsDescription__2">
                     <p>Un migrante nacional es “una persona que se desplaza o se ha desplazado dentro de un país, fuera de su lugar habitual de residencia independientemente de 1) su situación jurídica; 2) carácter voluntario o involuntario del desplazamiento; 3) las causas del desplazamiento; o 4) la duración de su estancia”.
-                    La migración interna hace referencia al movimiento de personas dentro de un país que conlleva al establecimiento de una nueva residencia temporal o permanente.2 Comprendiendo a aquellos nacionales o no nacionales que se muevan dentro de un país siempre que se trasladen fuera de su lugar de residencia habitual.
+                        La migración interna hace referencia al movimiento de personas dentro de un país que conlleva al establecimiento de una nueva residencia temporal o permanente.2 Comprendiendo a aquellos nacionales o no nacionales que se muevan dentro de un país siempre que se trasladen fuera de su lugar de residencia habitual.
                     </p>
                 </div>
             </div>
--- src/components/HealthMap/HealthMap.jsx
+++ src/components/HealthMap/HealthMap.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 import * as comunitiesData from './Hospitals.json'
 import AppContext from '../../App/AppContext';
+import { Button } from '@material-ui/core';
 
 const Map = () => {
     const [selectedComunity, setSelectedComunity] = useState(null);
     const { UseDataMapsComunity } = AppContext.Consumer();
     const [dataMapsComunity, setDataMapsComunity] = UseDataMapsComunity();
 
-    const {nombreComunidad, descripcionComunidad, direccionComunidad, telefonoComunidad, imgComunidad, email} = dataMapsComunity;
+    const { nombreComunidad, descripcionComunidad, direccionComunidad, telefonoComunidad, imgComunidad, email } = dataMapsComunity;
 
     useEffect(() => {
         setDataMapsComunity("");
@@ -29,9 +30,11 @@ const Map = () => {
                     }}
                     onClick={() => {
                         setSelectedComunity(comunities);
-                        setDataMapsComunity({ ...dataMapsComunity, descripcionComunidad:  comunities.properties.DESCRIPTIO, nombreComunidad:  comunities.properties.NAME,
-                            direccionComunidad:  comunities.properties.ADDRESS, telefonoComunidad:  comunities.properties.PHONE_NUMBER, 
-                            imgComunidad:  comunities.properties.PICTURE, email:  comunities.properties.EMAIL});
+                        setDataMapsComunity({
+                            ...dataMapsComunity, descripcionComunidad: comunities.properties.DESCRIPTIO, nombreComunidad: comunities.properties.NAME,
+                            direccionComunidad: comunities.properties.ADDRESS, telefonoComunidad: comunities.properties.PHONE_NUMBER,
+                            imgComunidad: comunities.properties.PICTURE, email: comunities.properties.EMAIL
+                        });
                     }}
                     icon={{
                         url: '/icons/Marker.png',
@@ -68,26 +71,50 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 const HealthMap = () => {
     const { useFaq } = AppContext.Consumer();
     const [type, Step] = useFaq();
+    const [healthPage, setHealthPage] = useState(0);
 
     useEffect(() => {
         Step("Faq");
     }, [])
 
 
-    return (
-        <div className="comunity">
-            <h1>Urgencia Medica</h1>
+    switch (healthPage) {
+        case 0:
+            return <div className="comunity">
+                <h1>Urgencia Medica</h1>
+                <div className="formDescription">
+                    <div className="formDescription__info">
+                        <div>
+                            <h3>¿Qué es una Urgencia médica?</h3>
+                            <p>Una urgencia es una alteración a tu integridad física o mental. Esta puede estar causada por un accidente, enfermedad o cualquier otro motivo por el cual requieras atención medica inmediata para disminuir los riesgos de invalidez y muerte. Debes acudir al hospital publico mas cercano para recibir atención.
+                                <br />
+                                A continuación te mostramos un mapa con los hospitales públicos mas cercanos a los cuales acudir </p>
+                            <Button className="formDescription__info__btn" key="1" variant="contained" color="primary" onClick={() => setHealthPage(1)}>
+                                Ir al mapa
+                            </Button>
+                        </div>
+                        <img className="formDescription__info__img" src="/images/refugiado.png" alt="" />
+                    </div>
+                </div>
 
-          
+            </div>
+            break;
 
-            <WrappedMap
-                googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDtYBdEwc_JoqUrA-GDkOwmv-K0YVrAoAw"}
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `100%` }} />}
-                mapElement={<div style={{ height: `100%` }} />} />
+        case 1:
+            return <div className="comunity">
+                <h1>Urgencia Medica</h1>
+                <WrappedMap
+                    googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDtYBdEwc_JoqUrA-GDkOwmv-K0YVrAoAw"}
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `100%` }} />}
+                    mapElement={<div style={{ height: `100%` }} />} />
 
-        </div>
-    )
+            </div>
+            break;
+
+        default:
+            break;
+    }
+    return <div></div>
 }
-
 export default HealthMap;
